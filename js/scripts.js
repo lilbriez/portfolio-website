14ba932ac22cbbb2dcd128770a952962e6e08b84
lilbriez <75446553+lilbriez@users.noreply.github.com>
2021-01-15 12:46:02 -0800

    Created function for rainbow menu icon to show on mobile screens

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Thanks for stopping by!"); //Custom page greeting
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
