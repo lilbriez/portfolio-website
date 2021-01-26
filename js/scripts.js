f937218f2e04ed0aed94ccbdd5bfd1a1951f894a
lilbriez <75446553+lilbriez@users.noreply.github.com>
2021-01-26 12:20:25 -0800

    Corfrected errors using Standard Javascript Style

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-console.log("Thanks for stopping by!"); //Custom page greeting
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
